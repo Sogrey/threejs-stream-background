71ad96e0632ec16193a23debff1cc7d51cc424d3
Sogrey <suzhaoheng2006@163.com>
2025-11-30 14:28:13 +0800

    更新 base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/threejs-stream-background/',
   plugins: [
     vue(),
     vueDevTools(),
